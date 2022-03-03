dfefc2d89b0e3fff55aad681acabb281ee7c9fd3
Kiana Dyson <kdyson@calpoly.edu>
2022-03-03 18:54:05 -0500

    Initial Commit

---
+++ MenuBarSafari/MenuBarSafariApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarSafariApp.swift
+//  MenuBarSafari
+//
+//  Created by Kiana Dyson on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarSafariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
